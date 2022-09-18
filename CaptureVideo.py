cafc4f11a32f02c4617723377768a05a9e8f55e0
harikalagara0006 <110987326+harikalagara0006@users.noreply.github.com>
2022-09-18 07:33:30 +0530

    Add files via upload

---
+++ CaptureVideo.py
@@ -0,0 +1,20 @@
+import cv2
+
+#Capture video from webcam
+vid_capture = cv2.VideoCapture(0)
+vid_cod = cv2.VideoWriter_fourcc(*'XVID')
+output = cv2.VideoWriter("C:/Users/Home/Downloads\Honeypot-Implementation-master/captured_video.mp4", vid_cod, 20.0, (640,480))
+
+while(True):
+     # Capture each frame of webcam video
+     ret,frame = vid_capture.read()
+     cv2.imshow("My cam video", frame)
+     output.write(frame)
+     # Close and break the loop after pressing "x" key
+     if cv2.waitKey(1) &0XFF == ord('x'):
+         break
+
+# close the already opened camera
+vid_capture.release()
+# close the already opened file
+output.release()
